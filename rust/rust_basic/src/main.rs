472274fe66efdcc40dcf1a95968acba03b113153
Chuyukang <2867908314@qq.com>
2021-03-13 13:54:28 +0000

    rust_basic update

--- rust/rust_basic/src/main.rs
+++ rust/rust_basic/src/main.rs
@@ -1,3 +1,6 @@
+// practice demo
+// link: https://doc.rust-lang.org/book/
+
 fn main() {
     // imutable and muttable
     let mut x = 5;
@@ -17,13 +20,34 @@ fn main() {
     let spaces = "   ";
     let spaces = spaces.len();
     println!("The length of spaces is: {}", spaces);
-    let mut spaces_mut = "   ";
+    // let mut spaces_mut = "   ";
     // spaces_mut = spaces_mut.len(); type is not same
-    let mut spaces_mut = spaces_mut.len();
-    println!("The length of spaces_mut is: {}", spaces_mut);
+    // let mut spaces_mut = spaces_mut.len();
+    // println!("The length of spaces_mut is: {}", spaces_mut);
 
 
     // Data Types
-    // 
+    // u32
+    let a: u32 = 2;
+    let b: u32 = 4;
+    let c = a/b;
+    println!("{}",c);
+    // char
+    let ch = "😻";
+    println!("{}",ch);
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("({}, {}, {})", x, y, z);
+    // array
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{}", arr[0]);
 
+    // function
+    another_function();
+}
+
+
+fn another_function() {
+    println!("Another function.");
 }
